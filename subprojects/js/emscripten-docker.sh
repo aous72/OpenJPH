bca3446de40b851554d3b78231bbda6f82aff1a3
Aous Naman <aous72@yahoo.com>
2019-12-15 12:26:42 +1100

    For vscode, modified the number of make threads 2 from 12. For emscripten_docker.sh, I added user id/group id.

--- subprojects/js/emscripten-docker.sh
+++ subprojects/js/emscripten-docker.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run -it -v "$(cd ../.. && pwd)":/src trzeci/emscripten /bin/bash
+docker run -it --user $(id -u):$(id -g) -v "$(cd ../.. && pwd)":/src trzeci/emscripten /bin/bash

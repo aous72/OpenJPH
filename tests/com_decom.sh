f0e846ce7c9b1d8be08243827f19052a3773a572
Aous Naman <aous72@yahoo.com>
2020-10-23 19:50:58 +1100

    Small addition to tests

--- tests/com_decom.sh
+++ tests/com_decom.sh
@@ -1,2 +1,21 @@
 #!/bin/bash
 
+export LD_LIBRARY_PATH=`pwd`/../:`\pwd`/../../bin
+export PATH=$PATH:`pwd`/../:`\pwd`/../../bin
+
+# compress and decompress the images
+kdu_compress $1
+ojph_expand $2
+
+# test PSNR and maximum error
+
+t1=$?
+
+rm test.jph
+rm test.ppm
+
+if [ $t1 = "0" ]; then
+  exit 0
+else
+  exit 1
+fi

b104e691cb2ae3ee13162a41045f41f57214a98f
Aous Naman <aous@unsw.edu.au>
2021-04-28 11:08:15 +1000

    Improving tests

--- tests/com_decom.sh
+++ tests/com_decom.sh
@@ -4,26 +4,52 @@ export LD_LIBRARY_PATH=`pwd`/../:`\pwd`/../../bin
 export PATH=$PATH:`pwd`/../:`\pwd`/../../bin
 
 # compress and decompress the images
-kdu_compress $1
-kdu_expand $2
-ojph_expand $3
+if [ "$1" = "-dec" -o "$1" = "-rdec"  ]; then
+  if ! kdu_compress $2; then
+    exit 1
+  fi
+elif [ "$1" = "-com" -o "$1" = "-rcom" ]; then
+  if ! ojph_compress $2; then
+    exit 1
+  fi
+else
+  exit 1
+fi
+if ! kdu_expand $3; then
+  exit 1
+fi
+if ! ojph_expand $4; then
+  exit 1
+fi
 
 # test PSNR and maximum error
-pwd
-out1=$(../psnr_pae $4 $5)
-out2=$(../psnr_pae $4 $6)
+out1=$(../psnr_pae $5 $6)
+if [ $? -ne 0 ]; then
+  exit 1
+fi
+out2=$(../psnr_pae $5 $7)
+if [ $? -ne 0 ]; then
+  exit 1
+fi
+
+rm $6 $7
 
 psnr1=$(echo $out1 | cut -f1 -d' ')
 psnr2=$(echo $out2 | cut -f1 -d' ')
 d1=$(bc -l <<< "$psnr1 - $psnr2")
 d1=${d1##*[+-]}
+
 pae1=$(echo $out1 | cut -f2 -d' ')
 pae2=$(echo $out2 | cut -f2 -d' ')
 d2=$(($pae1 - $pae2))
 d2=${d2##*[+-]}
 
-rm $5 $6
-
-# echo $d1 $d2
-
-exit `bc -l <<< "$d1 > 0.01 || $d2 > 1"`
+if [ "$1" = "-rcom" -o "$1" = "-rdec" ]; then
+  if [ "$psnr1" = "inf" -a "$psnr2" = "inf" -a "$pae1" = "0" -a "$pae2" = "0" ]; then
+    exit 0
+  else
+    exit 1
+  fi
+else
+  exit $(bc -l <<< "$d1 > 0.01 || $d2 > 1")
+fi

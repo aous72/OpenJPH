ec0ce78aef5bec9e004ae373de5e60b22e1806f2
Aous Naman <aous@unsw.edu.au>
2022-12-23 20:26:17 +1100

    This commit addresses too large or too small HT codeblock segments.

--- src/core/codestream/ojph_precinct.cpp
+++ src/core/codestream/ojph_precinct.cpp
@@ -472,11 +472,23 @@ namespace ojph {
 
             if (bb_read_bits(&bb, bits1, bit) == false)
             { data_left = 0; throw "error reading from file p9"; }
+            if (bit < 2) { 
+              throw "The cleanup segment of an HT codeblock cannot contain "
+                "less than 2 bytes";
+            }
+            if (bit >= 65535) {
+              throw "The cleanup segment of an HT codeblock must contain "
+                "less than 65535 bytes";
+            }
             cp->pass_length[0] = bit;
             if (num_passes > 1)
             {
               if (bb_read_bits(&bb, bits1 + extra_bit, bit) == false)
               { data_left = 0; throw "error reading from file p10"; }
+              if (bit >= 2047) {
+                throw "The refinement segment (SigProp and MagRep passes) of "
+                  "an HT codeblock must contain less than 2047 bytes";
+              }
               cp->pass_length[1] = bit;
             }
           }

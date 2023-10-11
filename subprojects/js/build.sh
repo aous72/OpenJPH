a15d3b34c11e974448a3f7b412c82fc532987545
Bill Wallace <wayfarer3130@gmail.com>
2023-10-11 13:44:34 -0400

    Change to debug build for a while

--- subprojects/js/build.sh
+++ subprojects/js/build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 mkdir -p build
-#(cd build && emcmake cmake -DCMAKE_BUILD_TYPE=Debug ..)
-(cd build && emcmake cmake ..)
+(cd build && emcmake cmake -DCMAKE_BUILD_TYPE=Debug ..)
+#(cd build && emcmake cmake ..)
 (cd build && emmake make VERBOSE=1 -j)

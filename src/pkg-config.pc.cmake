46da3dc409f9ffb82045610a1f7cfe99a165ad14
Aous Naman <aous@unsw.edu.au>
2024-07-04 16:48:02 +1000

    This is to address relative and absolute installation folders

--- src/pkg-config.pc.cmake
+++ src/pkg-config.pc.cmake
@@ -5,5 +5,5 @@ Requires: @PKG_CONFIG_REQUIRES@
 prefix=@CMAKE_INSTALL_PREFIX@
 includedir=@PKG_CONFIG_INCLUDEDIR@
 libdir=@PKG_CONFIG_LIBDIR@
-Libs: @PKG_CONFIG_LIBS@
-Cflags: @PKG_CONFIG_CFLAGS@
+Libs: -L${libdir} -lopenjph
+Cflags: -I${includedir}

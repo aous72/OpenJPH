03802f09e0ad326f16763517be9cafd3e83f7e8d
Aous Naman <aous72@yahoo.com>
2025-02-02 14:54:16 +1100

    Updating js subproject

--- subprojects/js/build.sh
+++ subprojects/js/build.sh
@@ -1,5 +1,9 @@
-#!/bin/sh
+#!/bin/bash
+
 mkdir -p build
-#(cd build && emcmake cmake -DCMAKE_BUILD_TYPE=Debug ..)
-(cd build && emcmake cmake ..)
-(cd build && emmake make VERBOSE=1 -j)
+cd build
+emcmake cmake .. -DCMAKE_BUILD_TYPE=Release -DOJPH_DISABLE_SIMD=ON && emmake make -j8 && mv libopenjph.* ../html/
+emcmake cmake .. -DCMAKE_BUILD_TYPE=Release -DOJPH_DISABLE_SIMD=OFF && emmake make -j8 && mv libopenjph.wasm ../html/libopenjph_simd.wasm
+cd ..
+sed 's/libopenjph.wasm/libopenjph_simd.wasm/g' build/libopenjph.js > html/libopenjph_simd.js
+rm build/libopenjph.js

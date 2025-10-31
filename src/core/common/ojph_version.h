81202530e1e485b44beb080726c7baafb79fb501
Aous Naman <aous72@yahoo.com>
2025-11-01 06:01:02 +1100

    Make aligned_alloc/free C functions (#227)
    This PR makes ojph_aligned_alloc and ojph_aligned_free C functions, called using extern "C" {}
    This is the correct way to support aligned_alloc which is a C11 function; it is only available in C++ 17, which we do not support.

--- src/core/common/ojph_version.h
+++ src/core/common/ojph_version.h
@@ -34,5 +34,5 @@
 //***************************************************************************/
 
 #define OPENJPH_VERSION_MAJOR 0
-#define OPENJPH_VERSION_MINOR 24
-#define OPENJPH_VERSION_PATCH 5
+#define OPENJPH_VERSION_MINOR 25
+#define OPENJPH_VERSION_PATCH 0

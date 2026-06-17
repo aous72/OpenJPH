bba5d8c6e5aba72a34ff52122ed1824d66415cc1
Aous Naman <aous@unsw.edu.au>
2026-06-17 17:50:44 +1000

    Another Try 3

--- src/core/openjph/ojph_simd_vsx.h
+++ src/core/openjph/ojph_simd_vsx.h
@@ -255,7 +255,7 @@ static inline v128_t vsx_i32x4_trunc_sat_f32x4(v128_t a)
 static inline v128_t vsx_f32x4_convert_i32x4(v128_t a)
 { return (v128_t)vec_ctf((vsx_v_i32)a, 0); }
 
-/*
+/**/
 //---------------------------------------------------------------------------
 // widening
 //---------------------------------------------------------------------------
@@ -287,38 +287,71 @@ static inline v128_t vsx_i64x2_extend_high_i32x4(v128_t a)
     c0,c1,c2,c3))
 #define vsx_i64x2_shuffle(a, b, c0,c1) \
   ((v128_t)__builtin_shufflevector((vsx_v_i64)(a), (vsx_v_i64)(b), c0,c1))
+
 */
 
+#if defined(__has_builtin)
+#  if __has_builtin(__builtin_shufflevector)
+#    define VSX_HAVE_SHUFFLEVECTOR 1
+#  endif
+#  if __has_builtin(__builtin_convertvector)
+#    define VSX_HAVE_CONVERTVECTOR 1
+#  endif
+#endif
+
+// ---------------------------------------------------------------------------
+// helper: typed load/store without aliasing
+// ---------------------------------------------------------------------------
+#define VSX_LOAD_TYPED(dst, src) memcpy(&(dst), &(src), sizeof(dst))
+#define VSX_STORE_TYPED(dst, src) memcpy(&(dst), &(src), sizeof(dst))
+
 //---------------------------------------------------------------------------
 // widening
 //---------------------------------------------------------------------------
+
 static inline v128_t vsx_i64x2_extend_low_i32x4(v128_t a)
 {
-#if defined(__clang__)
+#if defined(VSX_HAVE_SHUFFLEVECTOR) && defined(VSX_HAVE_CONVERTVECTOR)
   vsx_v_i32 v = (vsx_v_i32)a;
   return (v128_t)__builtin_convertvector(
     __builtin_shufflevector(v, v, 0, 1), vsx_v_i64);
-#elif defined(__GNUC__)
-  vsx_v_i32 v = (vsx_v_i32)a;
-  // Use vec_unpackl for widening (sign extend)
-  return (v128_t)vec_unpackl(v);
 #else
-  #error "Unsupported compiler"
+  vsx_v_i32 v;
+  int32_t in[4];
+  int64_t out[2];
+  vsx_v_i64 r;
+
+  VSX_LOAD_TYPED(v, a);
+  VSX_LOAD_TYPED(in, v);
+
+  out[0] = (int64_t)in[0];
+  out[1] = (int64_t)in[1];
+
+  VSX_STORE_TYPED(r, out);
+  return (v128_t)r;
 #endif
 }
 
 static inline v128_t vsx_i64x2_extend_high_i32x4(v128_t a)
 {
-#if defined(__clang__)
+#if defined(VSX_HAVE_SHUFFLEVECTOR) && defined(VSX_HAVE_CONVERTVECTOR)
   vsx_v_i32 v = (vsx_v_i32)a;
   return (v128_t)__builtin_convertvector(
     __builtin_shufflevector(v, v, 2, 3), vsx_v_i64);
-#elif defined(__GNUC__)
-  vsx_v_i32 v = (vsx_v_i32)a;
-  // Use vec_unpackh for widening (sign extend)
-  return (v128_t)vec_unpackh(v);
 #else
-  #error "Unsupported compiler"
+  vsx_v_i32 v;
+  int32_t in[4];
+  int64_t out[2];
+  vsx_v_i64 r;
+
+  VSX_LOAD_TYPED(v, a);
+  VSX_LOAD_TYPED(in, v);
+
+  out[0] = (int64_t)in[2];
+  out[1] = (int64_t)in[3];
+
+  VSX_STORE_TYPED(r, out);
+  return (v128_t)r;
 #endif
 }
 
@@ -326,7 +359,91 @@ static inline v128_t vsx_i64x2_extend_high_i32x4(v128_t a)
 // shuffles (immediate lane indices; 0..N-1 from a, N..2N-1 from b)
 //---------------------------------------------------------------------------
 
-#if defined(__clang__)
+static inline vsx_v_u8 vsx_i8x16_shuffle_fallback(
+    vsx_v_u8 a, vsx_v_u8 b,
+    int c0,int c1,int c2,int c3,int c4,int c5,int c6,int c7,
+    int c8,int c9,int c10,int c11,int c12,int c13,int c14,int c15)
+{
+  uint8_t aa[16], bb[16], rr[16];
+  vsx_v_u8 r;
+  int idx[16] = {
+    c0,c1,c2,c3,c4,c5,c6,c7,c8,c9,c10,c11,c12,c13,c14,c15
+  };
+
+  VSX_LOAD_TYPED(aa, a);
+  VSX_LOAD_TYPED(bb, b);
+
+  for (int i = 0; i < 16; ++i) {
+    int k = idx[i];
+    rr[i] = (k < 16) ? aa[k] : bb[k - 16];
+  }
+
+  VSX_STORE_TYPED(r, rr);
+  return r;
+}
+
+static inline vsx_v_i16 vsx_i16x8_shuffle_fallback(
+    vsx_v_i16 a, vsx_v_i16 b,
+    int c0,int c1,int c2,int c3,int c4,int c5,int c6,int c7)
+{
+  int16_t aa[8], bb[8], rr[8];
+  vsx_v_i16 r;
+  int idx[8] = { c0,c1,c2,c3,c4,c5,c6,c7 };
+
+  VSX_LOAD_TYPED(aa, a);
+  VSX_LOAD_TYPED(bb, b);
+
+  for (int i = 0; i < 8; ++i) {
+    int k = idx[i];
+    rr[i] = (k < 8) ? aa[k] : bb[k - 8];
+  }
+
+  VSX_STORE_TYPED(r, rr);
+  return r;
+}
+
+static inline vsx_v_i32 vsx_i32x4_shuffle_fallback(
+    vsx_v_i32 a, vsx_v_i32 b,
+    int c0,int c1,int c2,int c3)
+{
+  int32_t aa[4], bb[4], rr[4];
+  vsx_v_i32 r;
+  int idx[4] = { c0,c1,c2,c3 };
+
+  VSX_LOAD_TYPED(aa, a);
+  VSX_LOAD_TYPED(bb, b);
+
+  for (int i = 0; i < 4; ++i) {
+    int k = idx[i];
+    rr[i] = (k < 4) ? aa[k] : bb[k - 4];
+  }
+
+  VSX_STORE_TYPED(r, rr);
+  return r;
+}
+
+static inline vsx_v_i64 vsx_i64x2_shuffle_fallback(
+    vsx_v_i64 a, vsx_v_i64 b,
+    int c0,int c1)
+{
+  int64_t aa[2], bb[2], rr[2];
+  vsx_v_i64 r;
+  int idx[2] = { c0,c1 };
+
+  VSX_LOAD_TYPED(aa, a);
+  VSX_LOAD_TYPED(bb, b);
+
+  for (int i = 0; i < 2; ++i) {
+    int k = idx[i];
+    rr[i] = (k < 2) ? aa[k] : bb[k - 2];
+  }
+
+  VSX_STORE_TYPED(r, rr);
+  return r;
+}
+
+#if defined(VSX_HAVE_SHUFFLEVECTOR)
+
 #define vsx_i8x16_shuffle(a, b, c0,c1,c2,c3,c4,c5,c6,c7, \
                                  c8,c9,c10,c11,c12,c13,c14,c15) \
   ((v128_t)__builtin_shufflevector((vsx_v_u8)(a), (vsx_v_u8)(b), \
@@ -343,91 +460,23 @@ static inline v128_t vsx_i64x2_extend_high_i32x4(v128_t a)
 #define vsx_i64x2_shuffle(a, b, c0,c1) \
   ((v128_t)__builtin_shufflevector((vsx_v_i64)(a), (vsx_v_i64)(b), c0,c1))
 
-#elif defined(__GNUC__)
-// Helper inline functions for GCC instead of macros with compound statements
-static inline v128_t vsx_i8x16_shuffle_impl(v128_t a, v128_t b,
-    unsigned char c0, unsigned char c1, unsigned char c2, unsigned char c3,
-    unsigned char c4, unsigned char c5, unsigned char c6, unsigned char c7,
-    unsigned char c8, unsigned char c9, unsigned char c10, unsigned char c11,
-    unsigned char c12, unsigned char c13, unsigned char c14, unsigned char c15)
-{
-  vsx_v_u8 va = (vsx_v_u8)a;
-  vsx_v_u8 vb = (vsx_v_u8)b;
-  const unsigned char __attribute__((__vector_size__(16))) mask = {
-    c0, c1, c2, c3, c4, c5, c6, c7,
-    c8, c9, c10, c11, c12, c13, c14, c15
-  };
-  return (v128_t)vec_perm(va, vb, mask);
-}
+#else
 
-static inline v128_t vsx_i16x8_shuffle_impl(v128_t a, v128_t b,
-    int c0, int c1, int c2, int c3, int c4, int c5, int c6, int c7)
-{
-  vsx_v_i16 va = (vsx_v_i16)a;
-  vsx_v_i16 vb = (vsx_v_i16)b;
-  const unsigned char __attribute__((__vector_size__(16))) mask = {
-    (unsigned char)(((c0)&0x7)*2), (unsigned char)(((c0)&0x7)*2+1),
-    (unsigned char)(((c1)&0x7)*2), (unsigned char)(((c1)&0x7)*2+1),
-    (unsigned char)(((c2)&0x7)*2), (unsigned char)(((c2)&0x7)*2+1),
-    (unsigned char)(((c3)&0x7)*2), (unsigned char)(((c3)&0x7)*2+1),
-    (unsigned char)(((c4)&0x7)*2+16), (unsigned char)(((c4)&0x7)*2+1+16),
-    (unsigned char)(((c5)&0x7)*2+16), (unsigned char)(((c5)&0x7)*2+1+16),
-    (unsigned char)(((c6)&0x7)*2+16), (unsigned char)(((c6)&0x7)*2+1+16),
-    (unsigned char)(((c7)&0x7)*2+16), (unsigned char)(((c7)&0x7)*2+1+16)
-  };
-  return (v128_t)vec_perm(va, vb, mask);
-}
-
-static inline v128_t vsx_i32x4_shuffle_impl(v128_t a, v128_t b,
-    int c0, int c1, int c2, int c3)
-{
-  vsx_v_i32 va = (vsx_v_i32)a;
-  vsx_v_i32 vb = (vsx_v_i32)b;
-  const unsigned char __attribute__((__vector_size__(16))) mask = {
-    (unsigned char)(((c0)&0x3)*4), (unsigned char)(((c0)&0x3)*4+1),
-    (unsigned char)(((c0)&0x3)*4+2), (unsigned char)(((c0)&0x3)*4+3),
-    (unsigned char)(((c1)&0x3)*4), (unsigned char)(((c1)&0x3)*4+1),
-    (unsigned char)(((c1)&0x3)*4+2), (unsigned char)(((c1)&0x3)*4+3),
-    (unsigned char)(((c2)&0x3)*4+16), (unsigned char)(((c2)&0x3)*4+1+16),
-    (unsigned char)(((c2)&0x3)*4+2+16), (unsigned char)(((c2)&0x3)*4+3+16),
-    (unsigned char)(((c3)&0x3)*4+16), (unsigned char)(((c3)&0x3)*4+1+16),
-    (unsigned char)(((c3)&0x3)*4+2+16), (unsigned char)(((c3)&0x3)*4+3+16)
-  };
-  return (v128_t)vec_perm(va, vb, mask);
-}
-
-static inline v128_t vsx_i64x2_shuffle_impl(v128_t a, v128_t b,
-    int c0, int c1)
-{
-  vsx_v_i64 va = (vsx_v_i64)a;
-  vsx_v_i64 vb = (vsx_v_i64)b;
-  const unsigned char __attribute__((__vector_size__(16))) mask = {
-    (unsigned char)(((c0)&0x1)*8), (unsigned char)(((c0)&0x1)*8+1),
-    (unsigned char)(((c0)&0x1)*8+2), (unsigned char)(((c0)&0x1)*8+3),
-    (unsigned char)(((c0)&0x1)*8+4), (unsigned char)(((c0)&0x1)*8+5),
-    (unsigned char)(((c0)&0x1)*8+6), (unsigned char)(((c0)&0x1)*8+7),
-    (unsigned char)(((c1)&0x1)*8+16), (unsigned char)(((c1)&0x1)*8+1+16),
-    (unsigned char)(((c1)&0x1)*8+2+16), (unsigned char)(((c1)&0x1)*8+3+16),
-    (unsigned char)(((c1)&0x1)*8+4+16), (unsigned char)(((c1)&0x1)*8+5+16),
-    (unsigned char)(((c1)&0x1)*8+6+16), (unsigned char)(((c1)&0x1)*8+7+16)
-  };
-  return (v128_t)vec_perm(va, vb, mask);
-}
-
-// Macro wrappers that call the inline functions
 #define vsx_i8x16_shuffle(a, b, c0,c1,c2,c3,c4,c5,c6,c7, \
                                  c8,c9,c10,c11,c12,c13,c14,c15) \
-  vsx_i8x16_shuffle_impl((a), (b), c0,c1,c2,c3,c4,c5,c6,c7, \
-                                  c8,c9,c10,c11,c12,c13,c14,c15)
+  ((v128_t)vsx_i8x16_shuffle_fallback((vsx_v_u8)(a), (vsx_v_u8)(b), \
+    c0,c1,c2,c3,c4,c5,c6,c7,c8,c9,c10,c11,c12,c13,c14,c15))
 
 #define vsx_i16x8_shuffle(a, b, c0,c1,c2,c3,c4,c5,c6,c7) \
-  vsx_i16x8_shuffle_impl((a), (b), c0,c1,c2,c3,c4,c5,c6,c7)
+  ((v128_t)vsx_i16x8_shuffle_fallback((vsx_v_i16)(a), (vsx_v_i16)(b), \
+    c0,c1,c2,c3,c4,c5,c6,c7))
 
 #define vsx_i32x4_shuffle(a, b, c0,c1,c2,c3) \
-  vsx_i32x4_shuffle_impl((a), (b), c0,c1,c2,c3)
+  ((v128_t)vsx_i32x4_shuffle_fallback((vsx_v_i32)(a), (vsx_v_i32)(b), \
+    c0,c1,c2,c3))
 
 #define vsx_i64x2_shuffle(a, b, c0,c1) \
-  vsx_i64x2_shuffle_impl((a), (b), c0,c1)
+  ((v128_t)vsx_i64x2_shuffle_fallback((vsx_v_i64)(a), (vsx_v_i64)(b), c0,c1))
 
 #endif
 

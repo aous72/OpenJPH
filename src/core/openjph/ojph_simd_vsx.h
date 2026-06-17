99cfaa187425d6a25b29a50a1a11d2d6e10ff76f
Aous Naman <aous@unsw.edu.au>
2026-06-17 16:02:25 +1000

    Modify code to work on 3 Ubuntu platforms, and test them in CI

--- src/core/openjph/ojph_simd_vsx.h
+++ src/core/openjph/ojph_simd_vsx.h
@@ -255,6 +255,7 @@ static inline v128_t vsx_i32x4_trunc_sat_f32x4(v128_t a)
 static inline v128_t vsx_f32x4_convert_i32x4(v128_t a)
 { return (v128_t)vec_ctf((vsx_v_i32)a, 0); }
 
+/*
 //---------------------------------------------------------------------------
 // widening
 //---------------------------------------------------------------------------
@@ -286,6 +287,47 @@ static inline v128_t vsx_i64x2_extend_high_i32x4(v128_t a)
     c0,c1,c2,c3))
 #define vsx_i64x2_shuffle(a, b, c0,c1) \
   ((v128_t)__builtin_shufflevector((vsx_v_i64)(a), (vsx_v_i64)(b), c0,c1))
+*/
+
+//---------------------------------------------------------------------------
+// widening
+//---------------------------------------------------------------------------
+static inline v128_t vsx_i64x2_extend_low_i32x4(v128_t a)
+{
+    vsx_v_i32 v = (vsx_v_i32)a;
+    return (v128_t)vec_mergeh(v, (vsx_v_i32){0, 0, 0, 0});
+}
+
+static inline v128_t vsx_i64x2_extend_high_i32x4(v128_t a)
+{
+    vsx_v_i32 v = (vsx_v_i32)a;
+    return (v128_t)vec_mergel(v, (vsx_v_i32){0, 0, 0, 0});
+}
+
+//---------------------------------------------------------------------------
+// shuffles (immediate lane indices; 0..N-1 from a, N..2N-1 from b)
+//---------------------------------------------------------------------------
+#define vsx_i8x16_shuffle(a, b, c0,c1,c2,c3,c4,c5,c6,c7, \
+                                 c8,c9,c10,c11,c12,c13,c14,c15) \
+    ((v128_t)vec_perm((vsx_v_u8)(a), (vsx_v_u8)(b), \
+        (vsx_v_u8){c0,c1,c2,c3,c4,c5,c6,c7,c8,c9,c10,c11,c12,c13,c14,c15}))
+
+#define vsx_i16x8_shuffle(a, b, c0,c1,c2,c3,c4,c5,c6,c7) \
+    ((v128_t)vec_perm((vsx_v_u8)(a), (vsx_v_u8)(b), \
+        (vsx_v_u8){c0*2,c0*2+1,c1*2,c1*2+1,c2*2,c2*2+1,c3*2,c3*2+1, \
+                   c4*2,c4*2+1,c5*2,c5*2+1,c6*2,c6*2+1,c7*2,c7*2+1}))
+
+#define vsx_i32x4_shuffle(a, b, c0,c1,c2,c3) \
+    ((v128_t)vec_perm((vsx_v_u8)(a), (vsx_v_u8)(b), \
+        (vsx_v_u8){c0*4,c0*4+1,c0*4+2,c0*4+3, \
+                   c1*4,c1*4+1,c1*4+2,c1*4+3, \
+                   c2*4,c2*4+1,c2*4+2,c2*4+3, \
+                   c3*4,c3*4+1,c3*4+2,c3*4+3}))
+
+#define vsx_i64x2_shuffle(a, b, c0,c1) \
+    ((v128_t)vec_perm((vsx_v_u8)(a), (vsx_v_u8)(b), \
+        (vsx_v_u8){c0*8,c0*8+1,c0*8+2,c0*8+3,c0*8+4,c0*8+5,c0*8+6,c0*8+7, \
+                   c1*8,c1*8+1,c1*8+2,c1*8+3,c1*8+4,c1*8+5,c1*8+6,c1*8+7}))
 
 //---------------------------------------------------------------------------
 // swizzle: runtime byte-table lookup; lanes with index > 15 give 0
